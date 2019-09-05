28a44fa25182882d84707ca765956d7947904ac6
drbarq <J.Tustin@gmail.com>
2019-09-05 17:36:23 -0600

    extracted hook logic

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,40 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp'
+import useResults from '../hooks/useResuts'
 
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('')
-    const [results, setResults] = useState([])
-    const [errorMessage, setErrorMessage] = useState('')
-    // console.log(getEnvVars("a"))
-
-    const searchApi = async (searchTerm) => {
-        try {
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term: searchTerm,
-                    location: 'denver'
-                }
-            })
-            setResults(response.data.businesses)
-        } catch(err) {
-            setErrorMessage('Something went wrong')
-        }
-    }
-
-    // bad way:
-        // call searchApi when component is first rendered, creates an infinite loop
-    // searchApi('pasta')
-
-    // correct way:
-        // allow the searchApi to run once and only once due to the second argument
-    useEffect(() => {
-        searchApi('pasta')
-    }, [])
-
+    const [searchApi, results, errorMessage] = useResults()
+    
     return (
         <View>
             <SearchBar 
