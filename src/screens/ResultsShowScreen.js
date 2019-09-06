acb1dab54f19bdda6f5b7f2837c777332e36378a
drbarq <J.Tustin@gmail.com>
2019-09-06 11:02:33 -0600

    rendering images with flatlist

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native'
 import yelp from '../api/yelp'
 
 const ResultsShowScreen = props => {
@@ -16,14 +16,35 @@ const ResultsShowScreen = props => {
         getResult(id)
     }, [])
 
+    if (!result) {
+        return null
+    }
+
     return (
         <View>
-            <Text>Results Show</Text>
+            <Text>{result.name}</Text>
+            <FlatList
+                data={result.photos}
+                keyExtractor={photo => photo}
+                renderItem={ ({item}) => {
+                    return (
+                        <Image 
+                            style={styles.image}
+                            source={{uri: item}}
+                        />
+                    )
+                }}
+            />
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    image: {
+        height: 200,
+        width: 300
+    }
+})
 
 
 export default ResultsShowScreen
