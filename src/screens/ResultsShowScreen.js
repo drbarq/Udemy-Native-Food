98c6e3b5917a2b5a2d1e0984b5305ef1b1f9de71
drbarq <J.Tustin@gmail.com>
2019-09-06 10:52:57 -0600

    adding in additional dynamic fetches

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import yelp from '../api/yelp'
 
 const ResultsShowScreen = props => {
+    const [result, setResult]  = useState(null) 
     const id = props.navigation.getParam('id')
 
-    console.log(id)
+    console.log(result)
+
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`)
+        setResult(response.data)
+    }
+    useEffect(() => {
+        getResult(id)
+    }, [])
 
     return (
         <View>
